886f47da06d8716c96e677ad126d711f6d9c943c
Anna Melnikova <amelniko@student.42.fr>
2025-03-08 22:57:57 +0100

    Using virtual function for ClapTrap

--- ex02/ClapTrap.hpp
+++ ex02/ClapTrap.hpp
@@ -19,7 +19,7 @@ class ClapTrap
         ClapTrap(const ClapTrap &other);
         ClapTrap &operator=(const ClapTrap &other);
 
-        void attack(const std::string& target);
+        virtual void attack(const std::string& target);
         void takeDamage(unsigned int amount);
         void beRepaired(unsigned int amount);
 
