b190a4ac14184c26ef575465a5121a4d64ec62cd
Anna Melnikova <amelniko@student.42.fr>
2025-03-08 20:06:37 +0100

    ex01 is done

---
+++ ex01/ClapTrap.cpp
@@ -0,0 +1,128 @@
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(void) : _name("Default"), _hp(10), _energy(10), _attack_dmg(0)
+{
+    std::cout << "ClapTrap: Default constructor with no arguements is called" << std::endl;
+    return ;
+}
+
+ClapTrap::ClapTrap(std::string name) : _name(name), _hp(10), _energy(10), _attack_dmg(0)
+{
+    std::cout << "ClapTrap: Constructor with string as a parameter is called" << std::endl;
+    return ;
+}
+
+ClapTrap::~ClapTrap(void)
+{
+    std::cout << "ClapTrap: Destructor is called" << std::endl;
+    return ;
+}
+
+ClapTrap::ClapTrap(const ClapTrap& other) : _name(other._name), _hp(other._hp), _energy(other._energy), _attack_dmg(other._attack_dmg)
+{
+    std::cout << "ClapTrap: Copy constructor is called" << std::endl;
+    return ;
+}
+
+ClapTrap &ClapTrap::operator=(const ClapTrap& other)
+{
+    if (this != &other)
+    {
+        this->_name = other._name;
+        this->_hp = other._hp;
+        this->_energy = other._energy;
+        this->_attack_dmg = other._attack_dmg;
+    }
+    return (*this);
+}
+
+void ClapTrap::attack(const std::string& target)
+{
+    if (this->_hp == 0)
+    {
+        std::cout << "ClapTrap " << this->_name << " is dead and can't attack " << target << std::endl;
+        return ;
+    }
+    if (this->_energy == 0)
+    {
+        std::cout << "ClapTrap " << this->_name << " doesn't have enough energy to attack " << target << std::endl;
+        return ;
+    }
+    std::cout << "ClapTrap " << this->_name << " attacks " << target;
+    std::cout << ", causing " << this->_attack_dmg << " points of damage!" << std::endl;
+    this->_energy--;
+    return ;
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+    if (this->_hp == 0)
+    {
+        std::cout << this->_name << " is already dead" << std::endl;
+        return ;
+    }
+    if (this->_hp < amount)
+        this->_hp = 0;
+    else
+        this->_hp -= amount;
+    std::cout << "Now " << this->_name << " has " << this->_hp << " hit points of health" << std::endl;
+    return ;
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+    if (this->_hp == 0)
+    {
+        std::cout << this->_name << " is already dead and can't be repaired" << std::endl;
+        return ;
+    }
+    if (this->_energy == 0)
+    {
+        std::cout << this->_name << " doesn't have enough energy to be repaired" << std::endl;
+        return ;
+    }
+    std::cout << this->_name << " has been repaired and gets " << amount << " hit points back" << std::endl;
+    this->_hp += amount;
+    this->_energy--;
+    std::cout << "Now " << this->_name << " has " << this->_hp << " hit points of health" << std::endl;
+    return ;
+}
+
+void ClapTrap::setHealth(unsigned int amount)
+{
+    this->_hp = amount;
+    return ;
+}
+
+void ClapTrap::setEnergy(unsigned int amount)
+{
+    this->_energy = amount;
+    return ;
+}
+
+void ClapTrap::setDamage(unsigned int amount)
+{
+    this->_attack_dmg = amount;
+    return ;
+}
+
+std::string ClapTrap::getName(void) const
+{
+    std::string temp = this->_name;
+    return (temp);
+}
+
+unsigned int ClapTrap::getHealth(void) const
+{
+    return (this->_hp);
+}
+
+unsigned int ClapTrap::getEnergy(void) const
+{
+    return (this->_energy);
+}
+
+unsigned int ClapTrap::getDamage(void) const
+{
+    return (this->_attack_dmg);
+}
---
+++ ex01/main.cpp
@@ -0,0 +1,61 @@
+#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+
+int main(void)
+{
+    std::cout << "CLAPTRAP TEST" << std::endl;
+    {
+        ClapTrap first("Albert");
+        ClapTrap second("Shane");
+
+        std::cout << std::endl;
+        first.attack("Shane");
+
+        std::cout << std::endl;
+        second.takeDamage(3);
+
+        std::cout << std::endl;
+        second.beRepaired(5);
+
+        std::cout << std::endl;
+        for (int i = 0; i < 10; i++)
+            first.attack("Shane");
+
+        std::cout << std::endl;
+    }
+    std::cout << "-----------------------------------------------------" << std::endl;
+    std::cout << "SCAVTRAP TEST" << std::endl;
+    {
+        ScavTrap first("First");
+        ScavTrap second("Second");
+
+        std::cout << std::endl;
+        first.attack("Second");
+
+        std::cout << std::endl;
+        second.takeDamage(first.getDamage());
+
+        std::cout << std::endl;
+        second.beRepaired(10);
+
+        std::cout << std::endl;
+        second.guardGate();
+
+        std::cout << std::endl;
+        for (int i = 0; i < 50; i++)
+            first.attack("Second");
+
+        std::cout << std::endl;
+        std::cout << "Copying ScavTrap..." << std::endl;
+        ScavTrap copyScav(second);
+        copyScav.attack("Dummy Target");
+
+        std::cout << std::endl;
+        std::cout << "Assigning ScavTrap..." << std::endl;
+        ScavTrap assignedScav = first;
+        assignedScav.attack("Another Target");
+
+        std::cout << std::endl;
+    }
+    return (0);
+}
