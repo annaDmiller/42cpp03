215701d99829ae75ecfb56b8bb6b50d93ceaa05a
Ann Melnikova <amelniko@paul-f4Br8s3.clusters.42paris.fr>
2025-03-11 08:54:52 +0100

    Adding virtual keyword to functions in both headers

--- ex01/ScavTrap.hpp
+++ ex01/ScavTrap.hpp
@@ -10,11 +10,11 @@ class ScavTrap : public ClapTrap
     public:
         ScavTrap(void);
         ScavTrap(std::string name);
-        ~ScavTrap(void);
+        virtual ~ScavTrap(void);
         ScavTrap(const ScavTrap& other);
         ScavTrap& operator=(const ScavTrap& other);
         
-        void attack(const std::string& target);
+        virtual void attack(const std::string& target);
         void guardGate(void);
 
 };
