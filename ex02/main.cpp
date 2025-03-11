533aacd5f8246ed4937b6dc9e08c79f78595e77e
Ann Melnikova <amelniko@paul-f4Br8s3.clusters.42paris.fr>
2025-03-11 08:50:05 +0100

    Removal of unnessesary tests

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -30,45 +30,6 @@ int main(void)
         std::cout << std::endl;
     }
     std::cout << "-----------------------------------------------------" << std::endl;
-    std::cout << "SCAVTRAP TEST" << std::endl;
-    {
-        ScavTrap first("First");
-        ScavTrap second("Second");
-
-        std::cout << std::endl;
-        first.attack("Second");
-
-        std::cout << std::endl;
-        second.takeDamage(20);
-
-        std::cout << std::endl;
-        second.beRepaired(10);
-
-        std::cout << std::endl;
-        second.guardGate();
-
-        std::cout << std::endl;
-        for (int i = 0; i < 50; i++)
-            first.attack("Second");
-
-        std::cout << std::endl;
-        std::cout << "Copying ScavTrap..." << std::endl;
-        ScavTrap copyScav(second);
-        copyScav.attack("Dummy Target");
-
-        std::cout << std::endl;
-        std::cout << "Assigning ScavTrap..." << std::endl;
-        ScavTrap assignedScav = first;
-        assignedScav.attack("Another Target");
-
-        std::cout << std::endl;
-        second.takeDamage(100);
-        second.beRepaired(10);
-        second.attack("First");
-
-        std::cout << std::endl;
-    }
-    std::cout << "-----------------------------------------------------" << std::endl;
     std::cout << "FRAGTRAP TEST" << std::endl;
     {
         FragTrap first("First");
